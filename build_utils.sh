fee0028017c873089c54ea2db59508efb758ff9c
Hubert Dąbrowski <hubert.dabrowski@cyberark.com>
2025-11-13 15:05:06 +0100

    Add --platform flag to docker build, pull and push commands

--- build_utils.sh
+++ build_utils.sh
@@ -18,16 +18,41 @@ function gen_versions()
   done
 }
 
+# Returns the machine architecture for the current platform.
+# Takes into account the DOCKER_DEFAULT_PLATFORM environment variable.
+get_machine_architecture() {
+  local architecture
+  if [[ -n "${DOCKER_DEFAULT_PLATFORM:-}" ]]; then
+    # Take part after the slash (e.g. linux/amd64 -> amd64)
+    architecture="${DOCKER_DEFAULT_PLATFORM##*/}"
+  else
+    architecture="$(uname -m)"
+  fi
+
+  case "$architecture" in
+    x86_64 | amd64)
+      echo "linux/amd64"
+      ;;
+    aarch64 | arm64)
+      echo "linux/arm64"
+      ;;
+    *)
+      echo "linux/amd64"
+      ;;
+  esac
+}
+
 # tag_and_publish tag source image1 image2 ...
 function tag_and_push() {
   local tag="$1"; shift
   local source="$1"; shift
+  local arch=$(get_machine_architecture)
 
   for image in "$@"; do
     local target=$image:$tag
     echo "Tagging $source as $target and pushing..."
     docker tag "$source" "$target"
-    docker push "$target"
+    docker push --platform "$arch" "$target"
   done
 }
 
@@ -36,8 +61,8 @@ function prepare_manifest() {
   local image="$1"
   local tag="$2"
 
-  docker pull "${image}:${tag}-amd64"
-  docker pull "${image}:${tag}-arm64"
+  docker pull --platform linux/amd64 "${image}:${tag}-amd64"
+  docker pull --platform linux/arm64 "${image}:${tag}-arm64"
 
   docker manifest create \
     --insecure \
