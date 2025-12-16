8df08c656987b6ad5e816ba960b5bfb4c29fe628
Pawel Wozniak <pawel.wozniak@cyberark.com>
2025-12-16 13:45:56 +0100

    Fix migration issues when restoring from backup

--- db/migrate/20251212000000_clear_slosilo_cache_trigger.rb
+++ db/migrate/20251212000000_clear_slosilo_cache_trigger.rb
@@ -1,3 +1,34 @@
 # frozen_string_literal: true
 
-require 'slosilo_cache/migration'
+Sequel.migration do
+  table = :slosilo_keystore
+
+  up do
+    run <<~SQL
+        CREATE OR REPLACE FUNCTION slosilo_cache_notify()
+        RETURNS trigger
+        LANGUAGE plpgsql
+        AS $$
+        BEGIN
+        PERFORM pg_notify('clear_slosilo_cache', '');
+        RETURN NULL;
+      END;
+        $$;
+    SQL
+
+    run <<~SQL
+      DROP TRIGGER IF EXISTS slosilo_cache_clear_notify ON #{quote_identifier(table)};
+      CREATE TRIGGER slosilo_cache_clear_notify
+      AFTER UPDATE OR DELETE ON #{quote_identifier(table)}
+      FOR EACH STATEMENT
+      EXECUTE FUNCTION slosilo_cache_notify();
+    SQL
+  end
+
+  down do
+    run <<~SQL
+      DROP TRIGGER IF EXISTS slosilo_cache_clear_notify ON #{quote_identifier(table)};
+      DROP FUNCTION IF EXISTS slosilo_cache_notify();
+    SQL
+  end
+end
